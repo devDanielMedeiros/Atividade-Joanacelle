fc00b63a5817e166d0f86d36f95dcb8d31b27e4f
devDanielMedeiros <cmedeiros@cs.unipe.edu.br>
2026-04-08 20:32:23 -0300

    missão_27.py

---
+++ missão_27.py
@@ -0,0 +1,6 @@
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite outro número inteiro: "))
+if (a > 0) and (b > 0):
+    print("a soma dos números é", a + b)
+else:
+    print("o produto dos números é", a * b)
