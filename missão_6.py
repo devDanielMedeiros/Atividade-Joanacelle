1f6452672643fed8328f2f150f4e8c15edad20ec
devDanielMedeiros <cmedeiros@cs.unipe.edu.br>
2026-04-08 19:57:57 -0300

    missão_6.py

---
+++ missão_6.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite outro número inteiro: "))
+if (a > b):
+    print(f"O número {a} é maior que o número {b}.")
+elif a < b:
+    print(f"O número {b} é maior que o número {a}.")
+else:
+    print("Os números são iguais.")
