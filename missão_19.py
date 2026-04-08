f7be27f9fd94e9d8182f1f366512c03ae905cb9f
devDanielMedeiros <cmedeiros@cs.unipe.edu.br>
2026-04-08 20:25:45 -0300

    missão_19.py

---
+++ missão_19.py
@@ -0,0 +1,6 @@
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite outro número inteiro: "))
+if a == b:
+    print("Os números são iguais.")
+else:
+    print('A diferença entre os números é:', abs(a - b))
